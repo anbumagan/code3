ea174daf14ab6e1343a6fdcef45cb93758989976
anbumagan <50615722+anbumagan@users.noreply.github.com>
2019-06-10 20:15:09 -0700

    Create 73.py

---
+++ 73.py
@@ -0,0 +1,14 @@
+a=int(input())
+c,b=input().split()
+x=int(c)
+y=int(b)
+c1=d1=0
+for i in range(x,y):
+    if a==i:
+        c1=c1+1
+    else:
+        d1=d1+1
+if(c1>=1):
+    print("yes")
+else:
+    print("no")
