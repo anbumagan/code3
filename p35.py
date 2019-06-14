00446e600604e91249f91fbddbc4ca084f1eab5d
anbumagan <50615722+anbumagan@users.noreply.github.com>
2019-06-13 23:29:28 -0700

    Update p35.py

--- p35.py
+++ p35.py
@@ -1,9 +1,9 @@
 a=str(input())
 a1=a.replace(" ","")
-x=a1.lower()
+x=a1
 a2=[]
 for i in x:
     if(x.count(i)==1):
         a2.append(i)
-print(*a2,end=" ")     
+print(*a2,end=" ")         
         
